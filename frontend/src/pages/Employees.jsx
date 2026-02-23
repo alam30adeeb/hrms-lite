c422f46ab77d6b7a4a8534d18d4a26f77ff7e668
Adeeb <aadeeb771@gmail.com>
2026-02-24 00:43:39 +0530

    Fix CORS allow all for development

--- frontend/src/pages/Employees.jsx
+++ frontend/src/pages/Employees.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
 
-const API = "http://127.0.0.1:8000";
+const API = "https://hrms-backend-7fos.onrender.com/";
 
 function Employees() {
   const [employees, setEmployees] = useState([]);
